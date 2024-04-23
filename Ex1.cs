4af9246c7d6200f52d74786c2f9bf8c02e52c227
Алёна <111691760+alenoktee@users.noreply.github.com>
2024-04-23 12:04:36 +0300

    Update and rename Num1.cs to Ex1.cs

---
+++ Ex1.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Ex1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nExercise 1:\nEnter the string:");
+            string word = Console.ReadLine();
+            ScoreOfString(word);
+
+            Ex2 myObject = new Ex2();
+            myObject.Mimimu();
+            Console.ReadKey();
+        }
+        static void ScoreOfString(string s)
+        {
+            byte[] asciiBytes = Encoding.ASCII.GetBytes(s);
+            int bytik = asciiBytes[0];
+            int sum = 0;
+            for (int i = 1; i < asciiBytes.Length; i++)
+            {
+                int now = (int)asciiBytes[i];
+                int res = Math.Abs(bytik - now);
+                Console.WriteLine($"{bytik} - {now} = {res}");
+                bytik = now;
+                sum += res;
+            }
+            Console.WriteLine("Output: " + sum);
+        }
+    }
+}
